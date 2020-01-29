bc1ecb6c68bb004d77eae85c2ad0bf636f67b081
Varvara <varya1441@gmail.com>
2020-01-29 11:33:00 +0300

    there was a mistake

--- fitness-app-server/src/main/java/com/training/fitnessappserver/controller/ActivityController.java
+++ fitness-app-server/src/main/java/com/training/fitnessappserver/controller/ActivityController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/plans/")
+@RequestMapping("/api/v1/plans/activities")
 @CrossOrigin(origins = "http://localhost:3000")
 public class ActivityController {
     ActivityService activityService;
